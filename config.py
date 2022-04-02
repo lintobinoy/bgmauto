2f06a187e9d25ec9a6bad6b49af125be2e5260a1
lintobinoy <71056969+lintobinoy@users.noreply.github.com>
2022-04-02 17:43:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('FORCES_SUB')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
-DEV_CHANNEL = "https://t.me/proxbotz"
+DEV_CHANNEL = "https://t.me/proxbots"
 
 # MongoDB information
 DATABASE_URI = environ['DATABASE_URI']
